4f55635cd1d37596cc22385c94ece786aa9db69a
ben <ben@mcguinnessmedia.com>
2025-12-24 12:03:31 -0500

    feat: add page controller class

---
+++ src/Controllers/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MMM\Controllers;
+
+use Timber\Timber;
+use MMM\Models\Post;
+
+class PageController extends BaseController
+{
+  public function render(): void
+  {
+    $this->renderView(
+      'pages/page.twig',
+      [
+        'post' => Timber::get_post(get_the_ID(), Post::class),
+      ]
+    );
+  }
+}
