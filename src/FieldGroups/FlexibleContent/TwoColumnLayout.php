c0bfa5d9f2dba5b0d317c804f233af8923a6a4ac
ben <ben@mcguinnessmedia.com>
2025-12-30 14:18:04 -0500

    feat: add two-column flexible content layout

---
+++ src/FieldGroups/FlexibleContent/TwoColumnLayout.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MMM\FieldGroups\FlexibleContent;
+
+use MMM\FieldGroups\Partials\ContentPartial;
+use MMM\FieldGroups\Partials\MediaSelectorPartial;
+
+class TwoColumnLayout extends BaseLayout {
+  protected function getName(): string
+  {
+    return 'two-column';
+  }
+
+  protected function getLabel(): string
+  {
+    return 'Two Column';
+  }
+
+  protected function addFields(): void
+  {
+    $this->fields
+      ->addRadio('alignment', [
+        'label' => __('Alignment', 'mcguinnessmedia'),
+        'options' => [
+          'left' => __('Left', 'mcguinnessmedia'),
+          'right' => __('Right', 'mcguinnessmedia'),
+        ]]
+      )
+      ->addFields(ContentPartial::get())
+      ->addFields(MediaSelectorPartial::get());
+  }
+}
