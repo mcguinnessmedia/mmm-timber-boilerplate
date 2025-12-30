1394906b3be3f327b89fa7d372cdbe8865fd4cef
ben <ben@mcguinnessmedia.com>
2025-12-30 14:17:52 -0500

    feat: add base flexible content layout

---
+++ src/FieldGroups/FlexibleContent/BaseLayout.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MMM\FieldGroups\FlexibleContent;
+
+use StoutLogic\AcfBuilder\FieldsBuilder;
+use StoutLogic\AcfBuilder\FieldNameCollisionException;
+
+abstract class BaseLayout {
+  protected FieldsBuilder $fields;
+
+  abstract protected function getName(): string;
+  abstract protected function getLabel(): string;
+
+  /**
+   * @return void
+   * @throws FieldNameCollisionException
+   */
+  abstract protected function addFields(): void;
+
+  /**
+   * @return FieldsBuilder
+   * @throws FieldNameCollisionException
+   */
+  public function build(): FieldsBuilder
+  {
+    $this->fields = new FieldsBuilder($this->getName());
+    $this->addFields();
+    return $this->fields;
+  }
+
+  public function getLayoutConfig(): array
+  {
+    return [
+      'name' => $this->getName(),
+      'label' => $this->getLabel(),
+      'display' => $this->getDisplay(),
+      'min' => $this->getMin(),
+      'max' => $this->getMax(),
+    ];
+  }
+
+  protected function getDisplay(): string
+  {
+    return 'block';
+  }
+
+  protected function getMin(): string
+  {
+    return '';
+  }
+
+  protected function getMax(): string
+  {
+    return '';
+  }
+}
