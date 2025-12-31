d0d10217c4e97e0e1ed2199c5407e3eb12e509b8
ben <ben@mcguinnessmedia.com>
2025-12-31 08:41:30 -0500

    feat: give access to flexible content registry in Post model

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -3,11 +3,36 @@
 namespace MMM\Models;
 
 use Timber\Post as TimberPost;
+use MMM\Services\FlexibleContentRegistryService;
 
 class Post extends TimberPost
 {
+  /**
+   * Get ACF fields for the post.
+   * @return array|null
+   */
   public function acf(): ?array
   {
     return get_fields($this->ID);
+  }
+
+  /**
+   * Get components with their view paths.
+   * @return array
+   */
+  public function components(): array
+  {
+    $service = FlexibleContentRegistryService::getInstance();
+    $components = $this->acf()['components'] ?? [];
+
+    return array_map(function ($component) use ($service) {
+      $component['view'] = $service->getViewForLayout($component['acf_fc_layout']);
+      return $component;
+    }, $components);
+  }
+
+  public function getLayoutView(string $layoutName): ?string
+  {
+    return FlexibleContentRegistryService::getInstance()->getViewForLayout($layoutName);
   }
 }
