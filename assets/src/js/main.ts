d5c8e0f2f2cc5efc7d62999d43158fad7dba0ac1
ben <ben@mcguinnessmedia.com>
2026-01-05 16:56:42 -0500

    feat: add swiper instantiation loop

--- assets/src/js/main.ts
+++ assets/src/js/main.ts
@@ -1,1 +1,19 @@
+import Swiper from "swiper";
+import { Navigation, A11y } from "swiper/modules";
+
 import '@css/main.scss';
+
+if ( document.querySelector( '.swiper' ) ) {
+  const swiperInstances: HTMLElement[] = Array.from( document.querySelectorAll( '.swiper' ) );
+
+  for ( const instance of swiperInstances ) {
+    new Swiper( instance, {
+      modules: [Navigation, A11y],
+      loop: instance.dataset.sliderLoop === 'true',
+      navigation: instance.dataset.sliderNavigation === 'true' && {
+        nextEl: instance.querySelector( 'button.swiper-button-next' ) as HTMLElement,
+        prevEl: instance.querySelector( 'button.swiper-button-prev' ) as HTMLElement,
+      }
+    } )
+  }
+}
