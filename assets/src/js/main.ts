128ff75e9eded48a5949716772d5dd90129b7197
ben <ben@mcguinnessmedia.com>
2026-02-03 15:36:47 -0500

    fix: initialize swiper in DOMContentLoaded

--- assets/src/js/main.ts
+++ assets/src/js/main.ts
@@ -1,19 +1,21 @@
 import Swiper from "swiper";
-import { Navigation, A11y } from "swiper/modules";
+import {Navigation, A11y} from "swiper/modules";
 
 import '@css/main.scss';
 
-if ( document.querySelector( '.swiper' ) ) {
-  const swiperInstances: HTMLElement[] = Array.from( document.querySelectorAll( '.swiper' ) );
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.querySelector('.swiper')) {
+    const swiperInstances: HTMLElement[] = Array.from(document.querySelectorAll('.swiper'));
 
-  for ( const instance of swiperInstances ) {
-    new Swiper( instance, {
-      modules: [Navigation, A11y],
-      loop: instance.dataset.sliderLoop === 'true',
-      navigation: instance.dataset.sliderNavigation === 'true' && {
-        nextEl: instance.querySelector( 'button.swiper-button-next' ) as HTMLElement,
-        prevEl: instance.querySelector( 'button.swiper-button-prev' ) as HTMLElement,
-      }
-    } )
+    for (const instance of swiperInstances) {
+      new Swiper(instance, {
+        modules: [Navigation, A11y],
+        loop: instance.dataset.sliderLoop === 'true',
+        navigation: instance.dataset.sliderNavigation === 'true' && {
+          nextEl: instance.querySelector('button.swiper-button-next') as HTMLElement,
+          prevEl: instance.querySelector('button.swiper-button-prev') as HTMLElement,
+        }
+      })
+    }
   }
-}
+})
