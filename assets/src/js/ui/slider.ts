a37a9836b400262dc77af469a4dd95dc71c49795
ben <ben@mcguinnessmedia.com>
2026-02-03 17:11:42 -0500

    chore: add docblock

--- assets/src/js/ui/slider.ts
+++ assets/src/js/ui/slider.ts
@@ -2,6 +2,27 @@ import {qs, qsa} from '../core/dom.js';
 import Swiper from "swiper";
 import {A11y, Navigation} from "swiper/modules";
 
+/**
+ * Initializes all Swiper instances on the page.
+ * Configure via data properties.
+ *
+ * @example
+ * ```html
+ * <div class='swiper'
+ *   data-slider-navigation="true"
+ *   data-slider-loop="true"
+ * >
+ *   <div class='swiper-wrapper'>
+ *     <div class='swiper-slide'>
+ *       Slide content
+ *     </div>
+ *   </div>
+ *
+ *   <button class="swiper-button-prev"></button>
+ *   <button class="swiper-button-next"></button>
+ * </div>
+ * ```
+ */
 export function initSliders() {
   qsa<HTMLElement>('.swiper').forEach((instance) => {
     new Swiper(instance, {
