fb662ed65dd03948796a4affb0920c820b6cba02
ben <ben@mcguinnessmedia.com>
2026-02-14 16:16:26 -0500

    feat: add improved SEO fields

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -2,21 +2,28 @@
 
 namespace MMM\Controllers;
 
-use Timber\Timber;
+use MMM\Services\SeoDataService;
+use Timber\{Timber, Post};
 
 abstract class BaseController
 {
   protected array $context = [];
+  protected SeoDataService $seoService;
 
   public function __construct()
   {
     $this->context = Timber::context();
+    $this->seoService = new SeoDataService();
   }
 
   abstract public function render(): void;
 
   protected function renderView(string $template, array $data = []): void
   {
+    if (isset($data['post']) && $data['post'] instanceof Post) {
+      $data['seo'] = $this->seoService->getSeoData($data['post']);
+    }
+
     Timber::render(
       $template,
       array_merge($this->context, $data)
